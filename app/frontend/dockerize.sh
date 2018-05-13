b45c5eff0350818e6cf8545a6c8a203008528f3e
Piotr Oramus <piotroramus@mail.com>
2018-05-13 20:45:09 +0200

    Clean dockerize scripts

--- app/frontend/dockerize.sh
+++ app/frontend/dockerize.sh
@@ -3,7 +3,3 @@
 docker build -t poramus/simple_frontend . || exit 1
 
 docker push poramus/simple_frontend
-
-#docker stop simple_frontend
-#docker rm simple_frontend
-#docker run -d -p 5001:5000 --name simple_frontend --link simple_backend:backend poramus/simple_frontend
--- app/json-generator/dockerize.sh
+++ app/json-generator/dockerize.sh
@@ -3,7 +3,3 @@
 docker build -t poramus/json_generator . || exit 1
 
 docker push poramus/json_generator
-
-#docker stop json_generator
-#docker rm json_generator
-#docker run -d -p 5002:5002 --name json_generator poramus/json_generator
