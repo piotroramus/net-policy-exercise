ee0250289f15d2066950848ea8583647957378bc
Piotr Oramus <piotroramus@mail.com>
2018-06-01 15:02:46 +0200

    Add bandwidth policy for the client

--- create_contiv_policies.sh
+++ create_contiv_policies.sh
@@ -4,7 +4,9 @@ netctl tenant create TestTenant
 
 netctl network create --tenant TestTenant --subnet=10.1.1.0/24 --gateway=10.1.1.254 -e "vxlan" TestNet
 
-netctl group create -t TestTenant TestNet client
+netctl netprofile create -t TestTenant -b 100Kbps -d 6 -s 80 testProfile
+
+netctl group create -t TestTenant -n testProfile TestNet client
 netctl group create -t TestTenant TestNet backend
 netctl group create -t TestTenant TestNet frontend
 netctl group create -t TestTenant TestNet database
