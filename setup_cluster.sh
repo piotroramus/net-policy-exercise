37b16fd38ef3f4680bbdbfd02450e566e5fe0853
Piotr Oramus <piotroramus@mail.com>
2018-06-01 13:01:45 +0200

    Wait a bit while setting the cluster up

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -5,9 +5,17 @@ cd k8s
 # deploy pods in a specific order, so their IP addresses are predictable
 # this is a hack, because I can get service DNS working with contiv
 
+SLEEP_TIME=5
+
 kubectl create -f fixed_config.yml
+sleep $SLEEP_TIME
 kubectl create -f mongo.yml
+sleep $SLEEP_TIME
 kubectl create -f backend.yml
+sleep $SLEEP_TIME
 kubectl create -f frontend.yml
+sleep $SLEEP_TIME
 kubectl create -f generator.yml
+
+#sleep $SLEEP_TIME
 #kubectl create -f client.yml
