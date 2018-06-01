56e37f43b458b808c5b79713a29a7d275d4be823
Piotr Oramus <piotroramus@mail.com>
2018-06-01 15:30:13 +0200

    Add frontend to the netprofile group

--- create_contiv_policies.sh
+++ create_contiv_policies.sh
@@ -7,7 +7,7 @@ netctl network create --tenant TestTenant --subnet=10.1.1.0/24 --gateway=10.1.1.
 netctl netprofile create -t TestTenant -b 100Kbps -d 6 -s 80 testProfile
 
 netctl group create -t TestTenant -n testProfile TestNet client
+netctl group create -t TestTenant -n testProfile TestNet frontend
 netctl group create -t TestTenant TestNet backend
-netctl group create -t TestTenant TestNet frontend
 netctl group create -t TestTenant TestNet database
 netctl group create -t TestTenant TestNet other
