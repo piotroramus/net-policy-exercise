dda3d18ccfb8cb85b0fcbbecfad85614f83e2e48
Piotr Oramus <piotroramus@mail.com>
2018-06-08 19:19:22 +0200

    Add policies skeletons

--- create_contiv_policies.sh
+++ create_contiv_policies.sh
@@ -4,10 +4,16 @@ netctl tenant create TestTenant
 
 netctl network create --tenant TestTenant --subnet=10.1.1.0/24 --gateway=10.1.1.254 -e "vxlan" TestNet
 
+netctl policy create -t TestTenant clientPolicy
+netctl policy create -t TestTenant frontendPolicy
+netctl policy create -t TestTenant backendPolicy
+netctl policy create -t TestTenant dbPolicy
+netctl policy create -t TestTenant otherPolicy
+
 netctl netprofile create -t TestTenant -b 100Kbps -d 6 -s 80 testProfile
 
-netctl group create -t TestTenant -n testProfile TestNet client
-netctl group create -t TestTenant -n testProfile TestNet frontend
-netctl group create -t TestTenant TestNet backend
-netctl group create -t TestTenant TestNet database
-netctl group create -t TestTenant TestNet other
+netctl group create -t TestTenant -n testProfile - p clientPolicy TestNet client
+netctl group create -t TestTenant -n testProfile -p frontendPolicy TestNet frontend
+netctl group create -t TestTenant TestNet -p backendPolicy backend
+netctl group create -t TestTenant TestNet -p dbPolicy database
+netctl group create -t TestTenant TestNet -p otherPolicy other
