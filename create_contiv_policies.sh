f26404ff2c440fa55f7bcc3690b0b4d698c44775
Piotr Oramus <piotroramus@mail.com>
2018-06-11 19:02:07 +0200

    Create ingress network policies definitions

--- create_contiv_policies.sh
+++ create_contiv_policies.sh
@@ -19,7 +19,25 @@ netctl group create -t TestTenant -p backendPolicy TestNet backend
 netctl group create -t TestTenant -p dbPolicy TestNet database
 netctl group create -t TestTenant -n testProfile -p otherPolicy TestNet other
 
-# TODO: prepare policies for both ingress and egress traffic
-# TODO: I have to block all the traffic, but TCP on specific ports
+# TODO: prepare policies for outcoming traffic as well! This is in case someone hacks into the pod and want to send some sensible data
 # I have a nice example here: http://contiv.github.io/documents/networking/policies.html
-#netctl policy rule-add -t TestTenant -d in --protocol icmp --from-group backend --action deny dbPolicy 1
+
+# TODO: make it multiline to look it nicer :)
+# database policies
+netctl policy rule-add dbPolicy 1 -direction=in -action=deny -t TestTenant
+netctl policy rule-add dbPolicy 2 -direction=in -protocol=tcp -port=27017 -action=allow -priority=10 -t TestTenant --from-group backend
+
+# backend policies
+netctl policy rule-add backendPolicy 1 -direction=in -action=deny -t TestTenant
+netctl policy rule-add backendPolicy 2 -direction=in -protocol=tcp -port=5000 -action=allow -priority=10 -t TestTenant --from-group frontend
+
+# frontend policies
+netctl policy rule-add frontendPolicy 1 -direction=in -action=deny -t TestTenant
+netctl policy rule-add frontendPolicy 2 -direction=in -protocol=tcp -port=5001 -action=allow -priority=10 -t TestTenant --from-group client
+
+# client policies
+netctl policy rule-add clientPolicy 1 -direction=in -action=deny -t TestTenant
+
+# other applications policies
+netctl policy rule-add otherPolicy 1 -direction=in -action=deny -t TestTenant
+netctl policy rule-add otherPolicy 2 -direction=in -protocol=tcp -port=5002 -action=allow -priority=10 -t TestTenant --from-group client
