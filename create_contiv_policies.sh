ccc9cc974cd910ebdd49b80a8a196146f705dcce
Piotr Oramus <piotroramus@mail.com>
2018-06-11 18:25:47 +0200

    Remove contiv services are these are meant for docker and do not work as expected

--- create_contiv_policies.sh
+++ create_contiv_policies.sh
@@ -5,12 +5,6 @@ netctl tenant create TestTenant
 
 netctl network create --tenant TestTenant --subnet=10.1.1.0/24 --gateway=10.1.1.254 -e "vxlan" TestNet
 
-# TODO: if works maybe I can change the port to 5000:80:TCP, so it is more transparent
-netctl service create backend-service --network TestNet --tenant TestTenant --selector=app=backend --port 5000:5000:TCP
-netctl service create frontend-service --network TestNet --tenant TestTenant --selector=app=frontend --port 5001:5001:TCP
-netctl service create mongo-service --network TestNet --tenant TestTenant --selector=app=mongo --port 27017:27017:TCP
-netctl service create generator-service --network TestNet --tenant TestTenant --selector=app=generator --port 5002:5002:TCP
-
 netctl policy create -t TestTenant clientPolicy
 netctl policy create -t TestTenant frontendPolicy
 netctl policy create -t TestTenant backendPolicy
@@ -25,5 +19,7 @@ netctl group create -t TestTenant -p backendPolicy TestNet backend
 netctl group create -t TestTenant -p dbPolicy TestNet database
 netctl group create -t TestTenant -n testProfile -p otherPolicy TestNet other
 
+# TODO: prepare policies for both ingress and egress traffic
 # TODO: I have to block all the traffic, but TCP on specific ports
+# I have a nice example here: http://contiv.github.io/documents/networking/policies.html
 #netctl policy rule-add -t TestTenant -d in --protocol icmp --from-group backend --action deny dbPolicy 1
